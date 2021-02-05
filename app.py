f8863777d7ac3e3f0f27a25cc98afcd23b12f944
Ignl94 <theenthusedwriter@gmail.com>
2021-02-04 20:15:23 -0500

    #

--- app.py
+++ app.py
@@ -164,14 +164,14 @@ def image_filter():
 
         # TODO: Call `apply_filter()` on the file path & filter type
 
-        image_url = f'/static/images/{image.filename}'
+        image_url = f'./static/images/{image.filename}'
         apply_filter(image_url, filter_type)
 
         context = {
             # TODO: Add context variables here for:
             # - The full list of filter types
             # - The image URL
-            'filtersDict': filter_types_dict.keys(),
+            'filtersDict': filter_types,
             'imageUrl': image_url
         }
 
@@ -180,7 +180,7 @@ def image_filter():
     else:  # if it's a GET request
         context = {
             # TODO: Add context variable here for the full list of filter types
-            'filtersDict': filter_types_dict.keys()
+            'filtersDict': filter_types
         }
         return render_template('image_filter.html', **context)
 
@@ -203,6 +203,8 @@ def gif_search():
     if request.method == 'POST':
         # TODO: Get the search query & number of GIFs requested by the user, store each as a
         # variable
+        search_query = request.form.get('search_query')
+        gifs_amount = request.form.get('quantity')
 
         response = requests.get(
             TENOR_URL,
@@ -211,6 +213,9 @@ def gif_search():
                 # - 'q': the search query
                 # - 'key': the API key (defined above)
                 # - 'limit': the number of GIFs requested
+                'q': search_query,
+                'key': API_KEY,
+                'limit': gifs_amount
             })
 
         gifs = json.loads(response.content).get('results')
@@ -220,7 +225,7 @@ def gif_search():
         }
 
         # Uncomment me to see the result JSON!
-        # pp.pprint(gifs)
+        pp.pprint(gifs)
 
         return render_template('gif_search.html', **context)
     else:
